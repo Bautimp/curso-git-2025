261ea36667f5a956bebb61dd509c22ba0406744d
Fernando <fistoda97@gmail.com>
2025-12-04 22:26:33 -0300

    implementaciones de la funcion division

--- calculadora.c
+++ calculadora.c
@@ -27,6 +27,10 @@ int multiplicacion(int a, int b) {
     return(a*b);
 }
 
+int division(int a, int b){
+    return(a/b);
+}
+
 int main() {
     int opcion = 0, num1, num2, resultado;
     while (opcion!=4) {
